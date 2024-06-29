c593a697d41a1e497589c74d52d68b9ddb79b160
Fernando Galve Mauricio <fgalvem@yahoo.es>
2024-06-29 18:27:40 +0200

    Let's go party!
    As there are people, there is time to celebrate.
    
    Signed-off-by: Fernando Galve Mauricio <fgalvem@yahoo.es>

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("\twelcome everybody!");
+		System.out.println("\t\tLet's go party!");
 	}
 
 }
