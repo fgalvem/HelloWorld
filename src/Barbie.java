8aed23db33ee65dc71ff7b28ad3e3879221cc8ab
Fernando Galve Mauricio <fgalvem@yahoo.es>
2024-06-30 11:30:26 +0200

    Identify Barbie's speech
    Just put the name of the speaker before the text.
    
    Signed-off-by: Fernando Galve Mauricio <fgalvem@yahoo.es>

--- src/Barbie.java
+++ src/Barbie.java
@@ -2,6 +2,6 @@
 public class Barbie {
 
 	public static void talk() {
-		System.out.println("c'mon barbi let's go party! ;-)");
+		System.out.println("(BARBIE:) c'mon barbi let's go party! ;-)");
 	}
 }
