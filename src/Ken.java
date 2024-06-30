e984d11b0be67c40429401aa82b7187ce78fdfa3
Fernando Galve Mauricio <fgalvem@yahoo.es>
2024-06-30 19:42:16 +0200

    Self assess that Ken is cool
    Ken seems a bit narcisist
    
    Signed-off-by: Fernando Galve Mauricio <fgalvem@yahoo.es>

--- src/Ken.java
+++ src/Ken.java
@@ -1,8 +1,7 @@
 
 public class Ken {
-
 	public static void talk() {
-		System.out.println("(KEN: ) oh no! I'll rest on the beach tanning!");
+		System.out.println("(KEN: ) oh no! I'll rest on the sunny beach!");
+		System.out.println("(KEN: ) how cool am I!");
 	}
-	
 }
