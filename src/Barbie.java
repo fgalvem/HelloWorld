ec9ceccfa61a3716a355dbf9d9bfaa1ca3475506
Fernando Galve Mauricio <fgalvem@yahoo.es>
2024-06-30 21:02:27 +0200

    Go STEM Barbie, go STEM ;-)
    The model girl, the model career.
    
    Signed-off-by: Fernando Galve Mauricio <fgalvem@yahoo.es>

--- src/Barbie.java
+++ src/Barbie.java
@@ -3,5 +3,6 @@ public class Barbie {
 
 	public static void talk() {
 		System.out.println("(BARBIE:) c'mon barbi let's go party! ;-)");
+		System.out.println("(BARBIE) I'll be computer engineer!!!");
 	}
 }
