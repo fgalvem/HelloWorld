055f3a55d9af1f2871dfe49026b35a8c27c9e273
Fernando Galve Mauricio <fgalvem@yahoo.es>
2024-07-01 11:01:56 +0200

    main line
    Signed-off-by: Fernando Galve Mauricio <fgalvem@yahoo.es>

--- src/Ken.java
+++ src/Ken.java
@@ -2,6 +2,7 @@
 public class Ken {
 	public static void talk() {
 		System.out.println("(KEN: ) oh no! I'll rest on the sunny beach!");
+		System.out.println("main");
 		System.out.println("(KEN: ) how cool am I!");
 	}
 }
