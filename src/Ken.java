a3c3e1bdef1b99b7309315d3460394f01f431442
fgalvem <fgalvem@yahoo.es>
2024-07-07 18:33:46 +0200

    Ken is C-O-O-L
    Update Ken sentence from the GitHub interface
    
    Let's see how this works!

--- src/Ken.java
+++ src/Ken.java
@@ -3,6 +3,6 @@ public class Ken {
 	public static void talk() {
 		System.out.println("(KEN: ) oh no! I'll rest on the sunny beach!");
 		System.out.println("main MERGES 300");
-		System.out.println("(KEN: ) how cool am I!");
+		System.out.println("(KEN: ) how cool am I! H-O-W C-O-O-L!!!");
 	}
 }
